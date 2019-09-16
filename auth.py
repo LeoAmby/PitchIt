a5d029a1088bb0ce5cbafd92fb02e5e22f4ca886
Leo <leoamby@gmail.com>
2019-09-16 17:03:55 +0300

    add html files i.e user profile, signup, login, and logout

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for 
 
 
 auth = Blueprint('auth', __name__)
@@ -6,3 +6,18 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+
+# @auth.route('/login', methods=['POST'])
+# def login_post():
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# @uth.route('/signup', methods=['POST'])
+# def signup_post():
+
+@auth.route('/logout')
+def logout():
+    return redirect(url_for('main.index'))
