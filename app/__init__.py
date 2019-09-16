afed625419f5edb68e74c13234b15a2b70017426
Leo <leoamby@gmail.com>
2019-09-16 17:47:26 +0300

    reinstallation of flask-login and psycopg2 modules

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
+
+
+db = SQLAlchemy()
 
 
 def create_app():
+    app = Flask(__name__)
+
     app.config['SECRET_KEY'] = '0da924e0a1'
     app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///db.sqlite3'
 
     db.init_app(app)
+    
+
 
     from auth import auth as auth_blueprint
     app.register_blurprint(auth_blueprint)
