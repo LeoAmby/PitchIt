3c7440d12a6d4dfcec06feb1527eb5a3b2415379
Leo <leoamby@gmail.com>
2019-09-16 16:28:51 +0300

    generate and initiate the secret key in the defined create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 # from flask_bootstrap import Bootstrap
 # from config import config
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,7 +10,15 @@ db = SQLAlchemy(app)
 
 
 def create_app():
-    app.config.from_object(Config)
+    app.config['SECRET_KEY'] = '0da924e0a1'
+    app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///db.sqlite3'
+
+    db.init_app(app)
+
+    # from .auth import auth as auth_blueprint
+    # app.register_blurprint(auth_blueprint)
+
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
