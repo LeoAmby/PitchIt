f2846474810b24b93b05617a1fd3e0145352e871
Leo <leoamby@gmail.com>
2019-09-17 10:27:43 +0300

    import the app to the flask shell for db conection

--- app/models.py
+++ app/models.py
@@ -4,7 +4,9 @@ from . import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
+    # email = db.Column(db.string(100), unique = True)
     username = db.Column(db.String(200))
-
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
     def __repr__(self):
         return f'User {self.username}'
